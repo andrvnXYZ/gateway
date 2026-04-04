2775efe8a686a858367139ac8b8fd4114bda2b65
andrvnXYZ <anbapanares@gmail.com>
2026-04-04 08:00:38 +0800

    changing the route

--- routes/web.php
+++ routes/web.php
@@ -6,16 +6,18 @@ $router->get('/', function () use ($router) {
     return "Gateway is Running!";
 });
 
-// KINI DAPAT NAA SA GAWAS SA MIDDLEWARE
-// Gateway routes/web.php
-
-$router->post('register', 'GatewayController@handleRequest');
+// KINI NGA MGA ROUTES ANG DAPAT MA-IGO SA POSTMAN PARA SA LOGIN/REGISTER
 $router->post('login', 'GatewayController@handleRequest');
+$router->post('register', 'GatewayController@handleRequest');
 
+// KINI NGA GROUP PARA SA MGA ROUTES NGA KINAHANGLAN NA OG TOKEN
 $router->group(['middleware' => 'auth'], function () use ($router) {
+    
+    // Para sa Users (Site1)
     $router->addRoute(['GET', 'POST', 'PUT', 'DELETE'], 'users', 'GatewayController@handleRequest');
     $router->addRoute(['GET', 'POST', 'PUT', 'DELETE'], 'users/{any:.*}', 'GatewayController@handleRequest');
-    
+
+    // Para sa Rentals (Site2)
     $router->addRoute(['GET', 'POST', 'PUT', 'DELETE'], 'rentals', 'GatewayController@handleRequest');
     $router->addRoute(['GET', 'POST', 'PUT', 'DELETE'], 'rentals/{any:.*}', 'GatewayController@handleRequest');
 });
